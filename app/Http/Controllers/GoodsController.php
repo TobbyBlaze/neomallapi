9d7ea357056c6a563eccd2ef995848bc6ee08942
Muritala Tobi <muritala.mt@gmail.com>
2020-08-22 15:15:46 +0000

    22-8-2020

--- app/Http/Controllers/GoodsController.php
+++ app/Http/Controllers/GoodsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 // use Illuminate\Support\Facades\Auth;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 use Illuminate\Support\Facades\Storage;
 // use Illuminate\Database\Eloquent\Collection\visits;
