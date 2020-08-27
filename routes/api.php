4edb56492b1648951f4066c1aaff07cf9c1de8a8
Muritala Tobi <muritala.mt@gmail.com>
2020-08-27 04:11:30 +0000

    date

--- routes/api.php
+++ routes/api.php
@@ -62,6 +62,7 @@ Route::group([ 'prefix' => 'auth'], function (){
         // Route::any('storecart', 'CartsController@store');
         Route::post('storecart', 'CartsController@store');
         Route::post('storewish', 'WishController@store');
+        Route::any('deletecart/{id}', 'CartsController@destroy');
         Route::any('clearcart', 'CartsController@clear');
         Route::post('order', 'OrderController@store');
         Route::any ( 'found-all', 'FindController@all');
