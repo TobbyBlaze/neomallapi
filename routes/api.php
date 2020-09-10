9a9e6c5428d095647ec81edc964c8b9abe9fe875
Muritala Tobi <muritala.mt@gmail.com>
2020-09-10 11:44:22 +0000

    date

--- routes/api.php
+++ routes/api.php
@@ -115,10 +115,10 @@ Route::any ( 'search', 'FindController@all');
 Route::get('location', function () {
 
     // $ip = '50.90.0.1';
-    $ip = Request::ip();
+    $ip = \Request::ip();
     // $ip = request()->ip();
     $data = \Location::get($ip);
     // dd($data);
-    return response()->json($data);
+    return response()->json($ip);
    
 });
