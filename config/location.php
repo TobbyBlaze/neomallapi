3cecbb4f082ca6a96f80466aa8cf05e86a61fb5c
Muritala Tobi <muritala.mt@gmail.com>
2020-09-11 04:33:04 +0000

    date

--- config/location.php
+++ config/location.php
@@ -30,9 +30,11 @@ return [
 
         Stevebauman\Location\Drivers\IpInfo::class,
 
+        Stevebauman\Location\Drivers\MaxMind::class,
+
         Stevebauman\Location\Drivers\GeoPlugin::class,
 
-        Stevebauman\Location\Drivers\MaxMind::class,
+        
 
     ],
 
