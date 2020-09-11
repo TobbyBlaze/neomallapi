34ef3a32351e87fdb9b6230a72196157542ba262
Muritala Tobi <muritala.mt@gmail.com>
2020-09-11 05:18:09 +0000

    date

--- routes/api.php
+++ routes/api.php
@@ -116,10 +116,26 @@ Route::any ( 'search', 'FindController@all');
 
 Route::get('location', function () {
 
+    $ipaddress = '';
+    if (isset($_SERVER['HTTP_CLIENT_IP']))
+        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+    else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED'];
+    else if(isset($_SERVER['REMOTE_ADDR']))
+        $ipaddress = $_SERVER['REMOTE_ADDR'];
+    else
+        $ipaddress = 'UNKNOWN';
+
     // $ip = '50.90.0.1';
     // $ip = \Request::ip();
     $ip = request()->ip();
-    $data = \Location::get($ip);
+    $data = \Location::get($ipaddress);
     // dd($data);
     return response()->json($data);
    
