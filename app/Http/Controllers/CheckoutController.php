d8ad94b82da0711a9804d5817ac935e8fb16b036
Muritala Tobi <muritala.mt@gmail.com>
2020-09-16 17:06:32 +0000

    stripe

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -12,26 +12,27 @@ class CheckoutController extends Controller
 {
     public function charge(Request $request)
     {
-        dd($request->all());
+        // dd($request->all());
 
-        // try {
-        //     // Stripe::setApiKey(env('STRIPE_SECRET_KEY'));
-        //     Stripe::setApiKey('sk_test_51FyXZOIfMsn8pOBN2CZG6KzUcUgWlySQBGLW8rP9x7ZL5P32nIyuoPfSG3VPDDqylbhmFYGIF0tuV5whXP7nGeUS00lHF870xd');
+        try {
+            // Stripe::setApiKey(env('STRIPE_SECRET_KEY'));
+            Stripe::setApiKey('sk_test_51FyXZOIfMsn8pOBN2CZG6KzUcUgWlySQBGLW8rP9x7ZL5P32nIyuoPfSG3VPDDqylbhmFYGIF0tuV5whXP7nGeUS00lHF870xd');
         
-        //     $customer = Customer::create(array(
-        //         'email' => $request->stripeEmail,
-        //         'source' => $request->stripeToken
-        //     ));
+            $customer = Customer::create(array(
+                // 'email' => $request->stripeEmail,
+                'email' => 'blaze@mail.com',
+                'source' => $request->id
+            ));
         
-        //     $charge = Charge::create(array(
-        //         'customer' => $customer->id,
-        //         'amount' => 1999,
-        //         'currency' => 'usd'
-        //     ));
+            $charge = Charge::create(array(
+                'customer' => $request->card->id,
+                'amount' => 1999,
+                'currency' => 'usd'
+            ));
         
-        //     return 'Charge successful, you get the course!';
-        // } catch (\Exception $ex) {
-        //     return $ex->getMessage();
-        // }
+            return 'Charge successful, you get the course!';
+        } catch (\Exception $ex) {
+            return $ex->getMessage();
+        }
     }
 }
