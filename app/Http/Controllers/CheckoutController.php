a77933f3e5dc55493d99f6f0c42a41041530c0e9
Muritala Tobi <muritala.mt@gmail.com>
2020-09-18 05:07:51 +0000

    checkout

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -8,11 +8,17 @@ use Stripe\Stripe;
 use Stripe\Customer;
 use Stripe\Charge;
 
+use App\User;
+use App\Order;
+use Auth;
+use DB;
+
 class CheckoutController extends Controller
 {
     public function charge(Request $request)
     {
         // dd($request->all());
+        $user = Auth::user();
 
         try {
             // Stripe::setApiKey(env('STRIPE_SECRET_KEY'));
@@ -20,7 +26,7 @@ class CheckoutController extends Controller
         
             $customer = Customer::create(array(
                 // 'email' => $request->stripeEmail,
-                'email' => 'blaze@mail.com',
+                'email' => $user->email,
                 'source' => $request->id
             ));
         
@@ -30,8 +36,30 @@ class CheckoutController extends Controller
                 'amount' => 1999,
                 'currency' => 'usd'
             ));
+
+            $data = [
+                'user' => $user,
+                'customer'=>$customer,
+                'charge'=>$charge,
+            ];
+
+            $order = new Order;
         
-            return 'Charge successful, you get the course!';
+            $order->user_id = $user->id;
+            $order->first_name = $request->input('first_name');
+            $order->last_name = $request->input('last_name');
+            $order->country = $request->input('country');
+            $order->address1 = $request->input('address1');
+            $order->address2 = $request->input('address2');
+            $order->city = $request->input('city');
+            $order->state = $request->input('state');
+            $order->zip = $request->input('zip');
+            $order->phone = $request->input('phone');
+            $order->email = $request->input('email');
+
+            $order->save();
+        
+            return response()->json($data,200);
         } catch (\Exception $ex) {
             return $ex->getMessage();
         }
