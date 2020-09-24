499ace7dc768156a9b489f6fbe19951a5719d9dd
Muritala Tobi <muritala.mt@gmail.com>
2020-09-24 17:21:19 +0000

    profile

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Hash;
 use App\Good;
 use App\Cart;
 use App\User;
